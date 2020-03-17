13b1962c31f7c61572f3440d2870251d127653aa
Alison Law <alisparklaw@gmail.com>
2020-03-17 16:41:05 +0000

    latest.  post working.  delete wip

--- queries.sql
+++ queries.sql
@@ -33,5 +33,5 @@ postalcode='11122'
 where
 customerid = 92;
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
- hi
+
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
